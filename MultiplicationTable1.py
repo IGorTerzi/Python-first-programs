a1752f4c641aab3945d6bf4436fb9e99470ce198
Igor Terzi <terzi.igoriok92@gmail.com>
2022-02-12 10:04:49 +0200

    Task2 commit

--- MultiplicationTable1.py
+++ MultiplicationTable1.py
@@ -1,26 +1,7 @@
-print('\n')
-print('Таблица умножения')
-print('\n')
-for i in range(1, 11):
-    for k in range(2, 5):
-        print(f'{k} x {i} = {k * i}\t', end='')
-        if(i==11):
-            print('\t')
-    print('') 
-print('\n')
-
-for i in range(1,11):
-    for m in range(5, 8):
-        print(f'{m} x {i} = {m * i}\t', end='')
-        if(i==11):
-            print('\t')
-    print('') 
-print('\n')
-
-for i in range(1,11):   
-    for l in range(8, 11):
-        print(f'{l} x {i} = {l * i}\t', end='')
-        if(i==11):
-            print('\t')
-    print('') 
-print('\n')
+for i in range(1, 10, 3):
+    for j in range(1, 11):
+        print(
+            f'{i} x {j} = {i*j}\t', 
+            f'{i+1} x {j} = {(i+1)*j}\t',
+            f'{i+2} x {j} = {(i+2)*j}')
+    print('---------------------------------------------')
