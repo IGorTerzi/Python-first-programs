1501ab4946edbd86f7decf028a6a51df4d9cd400
Igor Terzi <terzi.igoriok92@gmail.com>
2022-02-16 20:24:13 +0200

    Task3 commit

--- Task3.py
+++ Task3.py
@@ -1,2 +1,9 @@
-a = sorted([1,6,3,8,2])
-print(a)
+my_list = [int(el) for el in input().split()]
+
+print("Array: ", my_list)
+
+my_list.sort()
+print("Array: ", my_list)
+
+my_list.sort(reverse=True)
+print("Array: ", my_list)
